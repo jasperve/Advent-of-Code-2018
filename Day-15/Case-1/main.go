9b7dac671392a22b096ccd52bf2c2b83f81fb8c8
Jasper <jasper@blackmesa.nl>
2018-12-15 21:02:45 +0100

    latest version day 15

--- Day-15/Case-1/main.go
+++ Day-15/Case-1/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"log"
 	"math"
+	"sort"
 )
 
 const (
@@ -21,6 +22,19 @@ type coordinate struct {
 	class int
 	attackPower int
 	hitPoints int
+	step int
+}
+
+type byPosition [][]*coordinate
+
+func (c byPosition) Len() int {
+	return len(c)
+}
+func (c byPosition) Swap(i, j int) {
+	c[i], c[j] = c[j], c[i]
+}
+func (c byPosition) Less(i, j int) bool {
+	return c[i][len(c)-1].y < c[j][len(c)-1].y || (c[i][len(c)-1].y == c[j][len(c)-1].y && c[i][len(c)-1].x < c[j][len(c)-1].x)
 }
 
 var coordinates map[int]map[int]*coordinate
@@ -28,8 +42,7 @@ var coordinates map[int]map[int]*coordinate
 func main() {
 
 	coordinates = make(map[int]map[int]*coordinate)
-	elfs := []*coordinate{}
-	goblins := []*coordinate{}
+	players := []*coordinate{}
 	
 	file, err := os.Open("input.txt")
 	if err != nil { log.Fatalln("Unable to open input file") }
@@ -50,12 +63,12 @@ func main() {
 				newCoordinate.class = ELF
 				newCoordinate.attackPower = 3
 				newCoordinate.hitPoints = 200
-				elfs = append(elfs, &newCoordinate)
+				players = append(players, &newCoordinate)
 			case 71:
 				newCoordinate.class = GOBLIN
 				newCoordinate.attackPower = 3
 				newCoordinate.hitPoints = 200
-				goblins = append(goblins, &newCoordinate)
+				players = append(players, &newCoordinate)
 			}
 			
 			row[x] = &newCoordinate
@@ -69,63 +82,69 @@ func main() {
 	// While there are still Goblins and Elfs
 	for {
 
-		for y := 0; y < len(coordinates); y++ {
+		for p := 0; p < len(players); p++ {
 
-			for x := 0; x < len(coordinates[y]); x++ {
+			for o := 0; o < len(players); o++ {
 
-				if coordinates[y][x].class == GOBLIN || coordinates[y][x].class == ELF {
+				if players[p].class == players[o].class { continue }
+				
+				//From this point on we need to calculate which opponent is closest
+				fmt.Printf("Calculating routes for %v on: %v, %v towards %v on %v, %v\n", players[p].class, players[p].x, players[p].y, players[o].class, players[o].x, players[o].y) 
+
+				leastSteps := 0
+				possibleRoutes := [][]*coordinate{}
+				calculateRoutes(players[p], players[p], players[o], &[]*coordinate{}, []*coordinate{}, &possibleRoutes, 0, &leastSteps)
 		
-					var opponents []*coordinate
-
-					if coordinates[y][x].class == GOBLIN { 
-						opponents = elfs 
-					} else if coordinates[y][x].class == ELF { 
-						opponents = goblins 
+				if len(possibleRoutes) > 0 {
+				
+					//Remove all the routes that are longer then the minimal route
+					for pR := 0; pR < len(possibleRoutes); pR++ {
+						if len(possibleRoutes[pR]) > leastSteps {
+							possibleRoutes = append(possibleRoutes[:pR], possibleRoutes[pR+1:]...)
+						}		
 					}
 					
-					for o := 0; o < len(opponents); o++ {
-
-						//From this point on we need to calculate which opponent is closest
-						fmt.Printf("Calculating routes for goblin/elf on: %v, %v towards goblin/elf on %v, %v\n", coordinates[y][x].x, coordinates[y][x].y, opponents[o].x, opponents[o].y) 
-
-						leastSteps := 0
-						possibleRoutes := [][]*coordinate{}
-						calculateRoutes(coordinates[y][x], coordinates[y][x], opponents[o], &[]*coordinate{}, []*coordinate{}, &possibleRoutes, &leastSteps)
-					
-						fmt.Println("least steps:", leastSteps)
-						amountSameRoutes := 0
-
-						for pR := 0; pR < len(possibleRoutes); pR++ {
-							if len(possibleRoutes[pR]) == leastSteps {
-								fmt.Println("-------------------------")
-								amountSameRoutes++
-								for pRR := 0; pRR < len(possibleRoutes[pR]); pRR++ {
-									fmt.Printf("%v, %v\n", possibleRoutes[pR][pRR].x, possibleRoutes[pR][pRR].y)
-								}			
-							}		
-						}
-
-						if(amountSameRoutes > 1) { fmt.Println(amountSameRoutes)}
-
+					//Sort the remaining possible routes by endpoint
+					sort.Sort(byPosition(possibleRoutes))
+				
+					if len(possibleRoutes[0]) > 1 {
+						fmt.Printf("Player on position x: %v, y: %v is moving to x: %v, y: %v\n\n", players[p].x, players[p].y, possibleRoutes[0][1].x, possibleRoutes[0][1].y)
+						players[p].x = possibleRoutes[0][1].x 
+						players[p].y = possibleRoutes[0][1].y
+						coordinates[players[p].y][players[p].x], coordinates[players[p].y][players[p].x] = possibleRoutes[0][1], players[p] 
 					}
+			
+				} else {
+					fmt.Println("NO MORE ROUTES FOUND!")
+					return
+				}
 
+				// Reset steps for each coordinate
+				for i := 0; i < len(coordinates); i++ {
+					for j := 0; j < len(coordinates[i]); j++ {
+						coordinates[i][j].step = 0
+					}
 				}
 
 			}
 
 		}
 
-		fmt.Println(len(elfs), len(goblins))
-		
-		break
-
+		return
+	
 	}
 
 }
 
-func calculateRoutes(currentPosition *coordinate, startPosition *coordinate, targetPosition *coordinate, passedPositions *[]*coordinate, currentRoute []*coordinate, possibleRoutes *[][]*coordinate, leastSteps *int) {
+func calculateRoutes(currentPosition *coordinate, startPosition *coordinate, targetPosition *coordinate, passedPositions *[]*coordinate, currentRoute []*coordinate, possibleRoutes *[][]*coordinate, step int, leastSteps *int) {
 
 	*passedPositions = append(*passedPositions, currentPosition)
+
+	if currentPosition.step == 0 || step <= currentPosition.step {
+		currentPosition.step = step
+	} else {
+		return
+	}
 
 	if *leastSteps != 0 {
 		if len(currentRoute) + int(math.Abs(float64(currentPosition.x-targetPosition.x))) + int(math.Abs(float64(currentPosition.y-targetPosition.y))) > *leastSteps { 
@@ -134,9 +153,6 @@ func calculateRoutes(currentPosition *coordinate, startPosition *coordinate, tar
 	}
 
 	if currentPosition == targetPosition {
-		/*for cR := 0; cR < len(currentRoute); cR++ {
-			fmt.Println("hier: ", currentRoute[cR].x, currentRoute[cR].y)
-		}*/
 		if len(currentRoute) < *leastSteps || *leastSteps == 0 {
 			*leastSteps = len(currentRoute)
 		}
@@ -199,10 +215,10 @@ func calculateRoutes(currentPosition *coordinate, startPosition *coordinate, tar
 			if directionPriority[dP] == currentRoute[cR] { nextPositionFound = true }
 		}
 		for pP := 0; pP < len(*passedPositions); pP++ {
-			if directionPriority[dP] == (*passedPositions)[pP] { nextPositionFound = true }
+			if directionPriority[dP] == (*passedPositions)[pP] && step >= (*passedPositions)[pP].step { nextPositionFound = true }
 		}
-		if nextPositionFound == false {
-			calculateRoutes(directionPriority[dP], startPosition, targetPosition, passedPositions, currentRoute, possibleRoutes, leastSteps)
+		if !nextPositionFound {
+			calculateRoutes(directionPriority[dP], startPosition, targetPosition, passedPositions, currentRoute, possibleRoutes, step+1, leastSteps)
 		}
 	}
 

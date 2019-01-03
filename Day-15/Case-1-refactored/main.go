54c35abddb7a763f2dd72d3e89f237beca625557
Jasper <jasper@blackmesa.nl>
2019-01-03 20:02:50 +0100

    nieuwe

--- Day-15/Case-1-refactored/main.go
+++ Day-15/Case-1-refactored/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"bufio"
 	"sort"
+	//"github.com/pkg/profile"
 )
 
 const (
@@ -18,6 +19,7 @@ type coordinate struct {
 	x int
 	y int
 }
+
 type byXY []coordinate
 func (c byXY) Len() int {
 	return len(c)
@@ -28,6 +30,7 @@ func (c byXY) Swap(i, j int) {
 func (c byXY) Less(i, j int) bool {
 	return c[i].y < c[j].y || (c[i].y == c[j].y && c[i].x < c[j].x)
 }
+
 type byHitPointsAndXY []coordinate
 func (c byHitPointsAndXY) Len() int {
 	return len(c)
@@ -51,6 +54,7 @@ type node struct {
 	steps int
 	target *player
 }
+
 type byStepsAndXY []*node
 func (c byStepsAndXY) Len() int {
 	return len(c)
@@ -62,7 +66,8 @@ func (c byStepsAndXY) Less(i, j int) bool {
 	return c[i].steps < c[j].steps || (c[i].steps == c[j].steps && (c[i].location.y < c[j].location.y || (c[i].location.y == c[j].location.y && c[i].location.x < c[j].location.x)))
 }
 
-var board map[coordinate]int
+
+var cave map[coordinate]int
 var players map[coordinate]*player
 var neighbours = []coordinate{ coordinate{x: 0, y: -1}, coordinate{x: -1, y: 0}, coordinate{x: 1, y: 0}, coordinate{x: 0, y: 1}}
 
@@ -70,40 +75,14 @@ var maxX, maxY int
 
 func main() {
 
-	file, _ := os.Open("input-ivo.txt")
+	//defer profile.Start(profile.CPUProfile, profile.ProfilePath(".")).Stop()
 
-	board = make(map[coordinate]int)
+	cave = make(map[coordinate]int)
 	players = make(map[coordinate]*player)
 
-	input := bufio.NewScanner(file)
-	for y := 0; input.Scan(); y++ {
-		if y > maxY {
-			maxY = y
-		}
-		for x, u := range input.Text() {
-			if x > maxX {
-				maxX = x
-			}
-			switch u {
-			case 35: // WALL FOUND
-				board[coordinate{x,y}] = wall
-				continue
-			case 69: // ElF FOUND
-				players[coordinate{x,y}] = &player{ class: elf, attackPower: 3, hitPoints: 200 }
-			case 71: // GOBLIN FOUND
-				//if x == 16 && y == 21 {
-				//	players[coordinate{x,y}] = &player{ class: goblin, attackPower: 3, hitPoints: 3 }
-				//} else {
-					players[coordinate{x,y}] = &player{ class: goblin, attackPower: 3, hitPoints: 200 }
-				//}
-			}
-			board[coordinate{x,y}] = empty
-
-		}
-	}
+	analyseCave("input-ivo.txt")
 
 	round := 0
-
 	for {
 
 		var playersKeys []coordinate
@@ -151,6 +130,11 @@ func main() {
 					}
 					
 					if !elfsFound || !goblinsFound {
+
+						if i == len(playersKeys) - 1 {
+							round++
+						}
+
 						fmt.Println("hitpoints left", totalHitPoints)
 						fmt.Println("rounds done:", round)
 						fmt.Println("hitpoints * round: ", totalHitPoints*round)
@@ -170,6 +154,35 @@ func main() {
 }
 
 
+func analyseCave(fileLocation string) {
+
+	file, _ := os.Open(fileLocation)
+	input := bufio.NewScanner(file)
+	for y := 0; input.Scan(); y++ {
+		if y > maxY {
+			maxY = y
+		}
+		for x, u := range input.Text() {
+			if x > maxX {
+				maxX = x
+			}
+			switch u {
+			case 35:
+				cave[coordinate{x,y}] = wall
+				continue
+			case 69:
+				players[coordinate{x,y}] = &player{ class: elf, attackPower: 3, hitPoints: 200 }
+			case 71:
+				players[coordinate{x,y}] = &player{ class: goblin, attackPower: 3, hitPoints: 200 }
+			}
+			cave[coordinate{x,y}] = empty
+
+		}
+	}
+
+}
+
+
 func locateTarget(startLocation coordinate) (route []coordinate) {
 	
 	startNode := node { location: startLocation, steps: 0 }
@@ -183,10 +196,15 @@ func locateTarget(startLocation coordinate) (route []coordinate) {
 
 		currentNode := openList[0]
 		openList = append([]*node{}, openList[1:]...)
+		// If the coordinate is already in the closed list
+		if _, ok := closedList[ coordinate { currentNode.location.x, currentNode.location.y }]; ok {
+			continue
+		}
+
 		closedList[ coordinate { currentNode.location.x, currentNode.location.y } ] = currentNode
 
 		// If the number of steps is higher then the shortest route found or we bump into a wall
-		if (currentNode.steps > shortestRoute && shortestRoute != 0) || board[currentNode.location] == wall {
+		if (currentNode.steps > shortestRoute && shortestRoute != 0) || cave[currentNode.location] == wall {
 			continue
 		}
 
@@ -204,20 +222,15 @@ func locateTarget(startLocation coordinate) (route []coordinate) {
 			continue
 		}
 
-		NEIGHBOURLOOP:
+		//NEIGHBOURLOOP:
 		for _, neighbour := range neighbours {
-
-			// If the coordinate is already in the closed list
-			if _, ok := closedList[ coordinate { currentNode.location.x + neighbour.x, currentNode.location.y + neighbour.y }]; ok {
-				continue
-			}
-
+		
 			// If the coordiante is already in the open list
-			for o := 0; o < len(openList); o++ {
+			/*for o := 0; o < len(openList); o++ {
 				if openList[o].location.x == currentNode.location.x + neighbour.x && openList[o].location.y == currentNode.location.y + neighbour.y {
 					continue NEIGHBOURLOOP
 				}
-			}
+			}*/
 
 			newNode := node { 
 				parent: currentNode,
@@ -274,7 +287,7 @@ func attackTarget(attackerLocation coordinate) coordinate {
 }
 
 
-func printBoard() {
+func printCave() {
 
 	for y := 0; y <= maxY; y++ {
 		for x := 0; x <=maxX; x++ {
@@ -285,9 +298,9 @@ func printBoard() {
 					fmt.Printf("G")
 				}
 			} else {
-				if board[ coordinate{ x, y }] == wall {
+				if cave[ coordinate{ x, y }] == wall {
 					fmt.Printf("#")
-				} else if board[ coordinate{ x, y }] == empty {
+				} else if cave[ coordinate{ x, y }] == empty {
 					fmt.Printf(".")
 				}
 			}
